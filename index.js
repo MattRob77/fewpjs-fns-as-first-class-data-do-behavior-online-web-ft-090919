0ab82ad3d6d37c13e0ad978b9b9944c7d85973c3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-12 18:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 function greet(time) { 
   let hour = parseInt(time.split(':')[0])
-  if (hour < 12)
+  if (hour < 12) { 
+    
   
   
 }
