36ef1d0c84b489856ba45e7cef90373d114dff88
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-12 18:56:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function greet(time) {
 /* Write your implementation of displayMessage() */
 function displayMessage(string) { 
   let greeting = document.querySelector('#greeting')
-  greetingInnerText
+  greeting
   
 } 
